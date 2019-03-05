07aafab1af95f2352ff30308a40f88abac6f8870
Drew del Rosario <drewdelrosario@gmail.com>
2019-03-04 22:33:47 -0800

    completed exercise5

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,11 +3,24 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      first: '',
+      last: ''
+    };
+    this.handleFirst = this.handleFirst.bind(this);
+  }
+  handleFirst(e) {
+    console.log(this);
+    console.log(e.target.value);
+    this.setState({
+      first: e.target.value
+    });
   }
   render() {
     return (
       <div>
-        {this.props.first} {this.props.last}
+        {this.state.first} {this.state.last} <br />
+        First: <input name="first" onChange={this.handleFirst} />
       </div>
     );
   }
