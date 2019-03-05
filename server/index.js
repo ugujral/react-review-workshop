c010ac7f80ff2abcfb1ef83d6fd49aa80f2402bc
Drew del Rosario <drewdelrosario@gmail.com>
2019-03-04 20:53:46 -0800

    correct typo in server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = 1337;
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-app.use(express.static(path.resolve(__dirname, './static')));
+app.use(express.static(path.resolve(__dirname, '../static')));
 
 app.listen(PORT, () => {
   console.log('App is listening to port: ', PORT);
