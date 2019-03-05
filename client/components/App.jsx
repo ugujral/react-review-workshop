aa34bee58082a2a15ca09b8fb8b8c88352bf4a37
Drew del Rosario <drewdelrosario@gmail.com>
2019-03-04 22:16:51 -0800

    completed exercise3

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = props => {
-  return <div>Hello from Component</div>;
+  return (
+    <div>
+      {props.first} {props.last}
+    </div>
+  );
 };
 
 export default App;
