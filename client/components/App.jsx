dfe051c48d72896c9fd0d587440a5119c5875570
Drew del Rosario <drewdelrosario@gmail.com>
2019-03-04 22:21:55 -0800

    completed exercise4

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const App = props => {
-  return (
-    <div>
-      {props.first} {props.last}
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        {this.props.first} {this.props.last}
+      </div>
+    );
+  }
+}
 
 export default App;
